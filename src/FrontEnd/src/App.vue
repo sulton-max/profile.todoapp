9b391696b9f07061be1424e5f9f4ff3c2ec3bce8
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2023-04-28 22:15:51 +0500

    #12 Created initial base modal component

--- src/FrontEnd/src/App.vue
+++ src/FrontEnd/src/App.vue
@@ -1,11 +1,19 @@
 <template>
-    <div id="app">
-
+    <div>
+        <AppDashboard />
+        <AuthModal />
     </div>
 </template>
 
 <script lang="ts">
+import AppDashboard from '@/views/Dashboard.vue';
+import AuthModal from "@/components/AuthModal.vue";
+
 export default {
-    name: 'App'
+    name: 'App',
+    components: {
+        AppDashboard,
+        AuthModal,
+    }
 }
 </script>
---
+++ src/FrontEnd/src/components/base/Modal.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="modal-background absolute top-0 left-0 right-0 min-h-screen bg-black bg-opacity-80 flex items-center justify-center">
+        <div class="modal-content w-full md:w-fit m-6 p-6 rounded-md bg-white">
+            <slot name="content"></slot>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'AppModal'
+}
+
+</script>
