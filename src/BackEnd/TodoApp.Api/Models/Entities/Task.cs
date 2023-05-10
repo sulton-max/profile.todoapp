efc8086a3771bd6b9c1c80eccadfdefa71186d15
Max <sultonbek.rakhimov@gmail.com>
2023-05-10 21:56:52 +0500

    Revert "#3 Created conceptualized task entity"
    This reverts commit 7c0e7e97e9d0838e668acefcaf8cd88096e0933f.

---
+++ src/BackEnd/TodoApp.Api/Models/Entities/Task.cs
@@ -0,0 +1,58 @@
+﻿using TodoApp.Api.Models.Common;
+using TodoApp.Api.Models.Enums;
+using TaskStatus = TodoApp.Api.Models.Enums.TaskStatus;
+
+namespace TodoApp.Api.Models.Entities;
+
+/// <summary>
+/// Represents a task with status
+/// </summary>
+public class Task : IEntity
+{
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the title of the task
+    /// </summary>
+    public string Title { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the description of the task
+    /// </summary>
+    public string Description { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets related category
+    /// </summary>
+    public string Category { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets current status
+    /// </summary>
+    public TaskStatus Status { get; set; }
+
+    /// <summary>
+    /// Gets or sets current priority
+    /// </summary>
+    public TaskPriority Priority { get; set; }
+
+    /// <summary>
+    /// Gets or sets the date of creation
+    /// </summary>
+    public DateTime CreatedDate { get; set; }
+
+    /// <summary>
+    /// Gets or sets the date of last update
+    /// </summary>
+    public DateTime UpdatedDate { get; set; }
+
+    /// <summary>
+    /// Gets or sets the due date
+    /// </summary>
+    public DateTime DueDate { get; set; }
+
+    /// <summary>
+    /// Related user Id
+    /// </summary>
+    public Guid AssignedUserId { get; set; }
+}
---
+++ src/BackEnd/TodoApp.Api/Models/Enums/TaskPriority.cs
@@ -0,0 +1,27 @@
+﻿namespace TodoApp.Api.Models.Enums;
+
+/// <summary>
+/// Represents task priority levels
+/// </summary>
+public enum TaskPriority
+{
+    /// <summary>
+    /// Indicates not necessary level
+    /// </summary>
+    Low,
+
+    /// <summary>
+    /// Indicates normal priority
+    /// </summary>
+    Normal,
+
+    /// <summary>
+    /// Indicates high importance level
+    /// </summary>
+    High,
+
+    /// <summary>
+    /// Indicates important task level
+    /// </summary>
+    Urgent
+}
