a29946205aba514e21e49a7ff4926bb20010b342
Max <sultonbek.rakhimov@gmail.com>
2023-05-10 22:50:02 +0500

    #18 Created logging broker

--- src/BackEnd/TodoApp.Api/Program.cs
+++ src/BackEnd/TodoApp.Api/Program.cs
@@ -1,4 +1,10 @@
+using TodoApp.Api.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.AddLogging();
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
