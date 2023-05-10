60f3f050e7c87038c39bea201b0c495a4003f11b
Max <sultonbek.rakhimov@gmail.com>
2023-05-11 00:39:07 +0500

    #20 Created database related exceptions

---
+++ src/BackEnd/TodoApp.Api/Models/Exceptions/Database/DatabaseException.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace TodoApp.Api.Models.Exceptions.Database;
+
+[Serializable]
+public class DatabaseException : Exception
+{
+    public DatabaseException()
+    {
+    }
+
+    public DatabaseException(string? message) : base(message)
+    {
+    }
+
+    public DatabaseException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    protected DatabaseException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
