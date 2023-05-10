5ed11732d4f8162604de3af4b9ca0120e5df0536
Max <sultonbek.rakhimov@gmail.com>
2023-05-10 21:41:23 +0500

    #2 Created conceptualized user entity

---
+++ src/BackEnd/TodoApp.Api/Models/Entities/User.cs
@@ -0,0 +1,46 @@
+﻿using TodoApp.Api.Models.Common;
+
+namespace TodoApp.Api.Models.Entities;
+
+/// <summary>
+/// Represents a user of the system
+/// </summary>
+public class User : IEntity
+{
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the email address
+    /// </summary>
+    public string EmailAddress { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the password 
+    /// </summary>
+    public string Password { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the username
+    /// </summary>
+    public string Username { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the first name
+    /// </summary>
+    public string FirstName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the last name
+    /// </summary>
+    public string LastName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets user creation date
+    /// </summary>
+    public DateTime CreatedDate { get; set; }
+
+    /// <summary>
+    /// Gets or sets user last update date
+    /// </summary>
+    public DateTime UpdatedDate { get; set; }
+}
