2b502a0d1674b2845b1916c7a726318ac9677524
Max <sultonbek.rakhimov@gmail.com>
2023-05-17 18:48:51 +0500

    #25 Created user foundation service contract

---
+++ src/BackEnd/TodoApp.Api/Services/Foundation/Users/UserService.cs
@@ -0,0 +1,11 @@
+﻿using TodoApp.Api.Brokers.DataStorage;
+using TodoApp.Api.Brokers.Logger;
+
+namespace TodoApp.Api.Services.Foundation.Users;
+
+public class UserService : IUserService
+{
+    public UserService(ILoggerBroker loggerBroker, IDataStorageBroker dataStorageBroker)
+    {
+    }
+}
---
+++ src/BackEnd/TodoApp.Unit/GeneratedData/UserData.cs
@@ -0,0 +1,22 @@
+﻿using Bogus;
+using TodoApp.Api.Models.Users;
+
+namespace TodoApp.Unit.GeneratedData;
+
+public class UserData
+{
+    private static readonly Faker<User> ValidUserDataFaker = new Faker<User>().RuleFor(x => x.Id, Guid.NewGuid())
+        .RuleFor(x => x.EmailAddress, x => x.Person.Email)
+        .RuleFor(x => x.Password, x => x.Person.UserName)
+        .RuleFor(x => x.Username, x => x.Person.UserName)
+        .RuleFor(x => x.FirstName, x => x.Person.FirstName)
+        .RuleFor(x => x.LastName, x => x.Person.LastName)
+        .RuleFor(x => x.CreatedDate, DateTime.Now)
+        .RuleFor(x => x.UpdatedDate, DateTime.Now.AddDays(10))
+        .RuleFor(x => x.UpdatedDate, DateTime.Now.AddDays(10));
+
+    public static IEnumerable<User> Generate(uint count)
+    {
+        return ValidUserDataFaker.Generate(10);
+    }
+}
